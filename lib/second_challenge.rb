598643cd70f86f3ce99060130f7eb5425d4d83ae
Candice Parker <lexus2424@gmail.com>
2019-04-29 12:16:19 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,31 +6,24 @@ def second_challenge
       :hero => {},
       :hero_friends => []
    },
+  },
     :capulet => {
-      :patriarch => {
-        :name => "Lord Capulet",
-        :age => "50"
-      },
-      :matriarch => {
-        :name => "Lady Capulet",
-        :age => "51"
-      },
-      :heroine => {
-        :name => "Juliet",
-        :age => "15"
-      },
-      :heroine_friends => [
-        {
-          :name => "Steven",
-          :age => "30",
-         :attitude => "confused"
-        },
-        {
-          :name => "Nurse",
-          :age => "44",
-          :attitude => "worried"
-          }
-       ]
+      :patriarch => {}
+        
+        
+      
+      :matriarch => {},
+        
+      
+      
+      :heroine => {},
+        
+        
+      
+      :heroine_friends => [],
+        
+         }
+       }
      }
    }
 end
