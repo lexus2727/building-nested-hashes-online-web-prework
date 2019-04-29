7454c8436d76431e290f7da14db5d3fc03b7e704
Candice Parker <lexus2424@gmail.com>
2019-04-29 12:01:40 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,28 +2,28 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
+        #:name => "Lord Montague",
+        #:age => "53"
       },
       :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
+        #:name => "Lady Montague",
+        #:age => "54"
       },
       :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
+       # :name => "Romeo",
+       # :age => "15",
+       # :status => "alive"
       },
       :hero_friends => [
         {
-         # :name => "Benvolio",
-         # :age => "17",
-          #:attitude => "worried"
+         :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
           },
           {
-            #:name => "Mercutio",
-            #:age => "18",
-            #:status => "hot-headed"
+            :name => "Mercutio",
+            :age => "18",
+            :status => "hot-headed"
           }
         ]
     },
